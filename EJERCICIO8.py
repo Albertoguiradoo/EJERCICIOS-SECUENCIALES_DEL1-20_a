78a7a7e0430a80d5866282530cb388b73d73e7f0
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2022-09-30 23:09:44 +0200

    falta el 7 por terminar

---
+++ EJERCICIO8.py
@@ -0,0 +1,6 @@
+#Un vendedor recibe un sueldo base mas un 10% extra por comisión de sus ventas,.
+
+sueldo= 0
+print("Vamos a calcular tu sueldo con el 10% extra")
+sueldo= (int)(input("Dime cuanto cobras:\n "))
+print(" Tu sueldo con los extras es de un total de",(sueldo+ sueldo*0.1), "euros")
