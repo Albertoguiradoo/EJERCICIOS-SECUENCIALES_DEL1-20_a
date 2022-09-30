eb9aecedf1775771da0652514871df74834b52d8
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2022-09-30 23:09:44 +0200

    falta el 7 por terminar

---
+++ EJERCICIO7.py
@@ -0,0 +1,8 @@
+#Realiza un programa que reciba una cantidad de minutos y muestre por pantalla
+#a cuantas horas y minutos corresponde.Por ejemplo: 1000 minutos son 16 horas y 40 minutos.
+
+minutos = 0
+
+print("vamos a calcular minutos")
+minutos= (int) (input("¿Cuántos minutos deseas pasar a horas?:\n "))
+print("son:", (minutos/60), "minutos")
