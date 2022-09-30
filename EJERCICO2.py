27f144ca96033a5610b3eaf4beff59e6265a88be
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2022-09-30 11:11:51 +0200

    Ejercicio 2 a medias

---
+++ EJERCICO2.py
@@ -0,0 +1,13 @@
+#Calcular el perímetro y área de un rectángulo dada su base y su altura.
+
+from email.mime import base
+
+
+altura = 0
+
+lado = 0
+altura = 0
+
+lado= (int)(input("dime la distancia del lado :\n "))
+altura= (int)(input("dime la distancia de la altura :\n "))
+print("el perimetro del rectángulo es:")
