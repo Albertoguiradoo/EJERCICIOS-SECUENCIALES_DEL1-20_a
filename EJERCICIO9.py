a54f1fe040ef705297d7ac0c8400b42205a4fe1c
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2022-10-03 10:25:54 +0200

    Ejercicio 9 terminado

--- EJERCICIO9.py
+++ EJERCICIO9.py
@@ -10,16 +10,18 @@ producto2 = 0
 producto3 = 0
 
 productos= (int)(input("(¿Cuántos productos lleva usted en la cesta:\n  ?"))
-if (productos=1):
+if (productos==1):
     producto1=(int)(input("¿Cuánto vale el producto 1:\n?"))
-print("su comprasería de", (producto1*0.85),"euros con el descuento incluido")
-if (productos=2):
+    print("su compra sería de", (producto1*0.85),"euros con el descuento incluido")
+
+if (productos==2):
     producto1=(int)(input("¿Cuánto vale el producto 1:\n?"))
     producto2=(int)(input("¿Cuánto vale el producto 2:\n?"))
-print("su comprasería de", ((producto1+producto2)*0.85),"euros con el descuento incluido")
-If (productos=3):
-     producto1=(int)(input("¿Cuánto vale el producto 1:\n?"))
+    print("su comprasería de", ((producto1+producto2)*0.85),"euros con el descuento incluido")
+
+if (productos==3):
+    producto1=(int)(input("¿Cuánto vale el producto 1:\n?"))
     producto2=(int)(input("¿Cuánto vale el producto 2:\n?"))
     producto3=(int)(input("¿Cuánto vale el producto 3:\n?"))
-print("su comprasería de", ((producto1+producto2+producto3)*0.85),"euros con el descuento incluido")
+    print("su comprasería de", ((producto1+producto2+producto3)*0.85),"euros con el descuento incluido")
 
